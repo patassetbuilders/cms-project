ac2e3407c9cd14d24178f0dba0905626adb7edac
Pat James <pjames@assetbuilders.com.au>
2016-10-05 07:17:50 +1000

    removing blog comments

--- cms.rb
+++ cms.rb
@@ -5,15 +5,13 @@ require "tilt/erubis"
 require "pry"
 require "pry-byebug"
 
-#blog https://www.blogger.com/blogger.g?blogID=3808378357003388534#editor/target=page;pageID=7961129763412562467
+#blogg http://cms-project.blogspot.com.au/
 
 root = File.expand_path("..", __FILE__) #"/Users/Pat/code/Launch-School/cms-project"
 
 get "/" do
-  @contents = Dir.glob(root + "/data/*"),map do |path|
+  @contents = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end 
-  #using the 'entries' method will also show hidden files
-  # @contents = Dir.entries("data")
   erb :index
 end
