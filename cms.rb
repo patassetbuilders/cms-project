33f466ed173d25061af2ce3d322bad7e0c1b7bab
Pat James <pjames@assetbuilders.com.au>
2016-10-18 05:41:52 +1000

    adding file not found

--- cms.rb
+++ cms.rb
@@ -7,17 +7,34 @@ require "pry-byebug"
 
 #blogg http://cms-project.blogspot.com.au/
 
+
+configure do
+  enable :sessions
+  set :session_secret, 'cmc-secret'
+  #set :erb, :escape_html => true
+end
+
 root = File.expand_path("..", __FILE__) #"/Users/Pat/code/Launch-School/cms-project"
 
 get "/" do
-  @contents = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end 
+  cms_contents
   erb :index
 end
 
 get "/read_file/:file_name" do
-  file = root + "/data/" + params[:file_name]
-  @file_contents = File.read(file)  #beware of long files
-  erb :show_file
+  if cms_contents.include? params[:file_name]
+    file = root + "/data/" + params[:file_name]
+    @file_contents = File.read(file)  #beware of long files
+    erb :show_file
+  else
+    session[:message] = "#{params[:file_name]} does not exist."
+    redirect "/"
+  end
+end
+
+def cms_contents
+  root = File.expand_path("..", __FILE__)
+  @contents = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end 
 end
--- test/cms-project_test.rb
+++ test/cms-project_test.rb
@@ -27,4 +27,15 @@ class AppTest < Minitest::Test
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "Showing the contemts of changes.txt"
   end
+  
+  def test_document_not_found
+    get "/notafile.ext"
+    
+    assert_equal 302, last_response.status
+    
+    get last_response["Location"]
+    
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "/notafile.ext does not exist."
+  end
 end
