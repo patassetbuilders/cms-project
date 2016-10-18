06987ea9acf2b47ed11d264d6d4284b0a02765ab
Pat James <pjames@assetbuilders.com.au>
2016-10-19 07:16:31 +1000

    refactoring for improved readability

--- cms.rb
+++ cms.rb
@@ -22,9 +22,9 @@ get "/" do
 end
 
 get "/:file_name" do
-  if cms_contents.include? params[:file_name]
-    file_path = root + "/data/" + params[:file_name]
-    @file_contents = File.read(file_path)  #beware of long files
+  if file_exists?
+    retrieve_file_contents
+    render_file
     erb :show_file
   else
     session[:message] = "#{params[:file_name]} does not exist."
@@ -32,9 +32,37 @@ get "/:file_name" do
   end
 end
 
+get "/:file_name/edit" do
+  file_path = root + "/data/" + params[:file_name]
+  @file_name = params[:file_name]
+  @content = File.read(file_path)
+  
+  erb :edit_file
+end
+
+
 def cms_contents
   root = File.expand_path("..", __FILE__)
   @contents = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end 
+end
+
+def file_exists?
+  cms_contents.include? params[:file_name]
+end
+
+def retrieve_file_contents
+  root = File.expand_path("..", __FILE__) #"/Users/Pat/code/Launch-School/cms-project"
+  file_path = root + "/data/" + params[:file_name]
+  @file_contents = File.read(file_path)  #beware of long files
+end
+
+def render_file
+  if params[:file_name].reverse.start_with?('dm.')
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(@file_contents)
+  elsif params[:file_name].reverse.start_with?('.txt')
+    @file_contents
+  end
 end
